fe0842eb54b057a26e251f9c455107130ec43b41
muhammadalvin92 <alvinbomber87@gmail.com>
2024-02-05 18:03:18 +0700

    [ ADDING ] Stisla Template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Pages');
+    return view('index');
+});
+Route::get('/pages', function () {
+    return view('pages');
 });
